2c14e3b9b35bac6b38d027e7378a41ed7d2b69d2
Arafat Islam Zihad <arafat.zihad01@gmail.com>
2025-06-24 22:28:22 +0600

    Daynamic memory allocation

---
+++ Semester_1/Intro_to_cpp_for_dsa/dynamic_array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int *arr = new int[5];
+    for (int i = 1; i <= 5; i++){
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i <= 5; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    return 0;
+}
