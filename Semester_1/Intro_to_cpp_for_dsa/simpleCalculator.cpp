ecf347a0c1a2d1ab2b458cbb2bfa05249fe84204
Arafat Islam Zihad <arafat.zihad01@gmail.com>
2025-06-25 22:01:16 +0600

    simple calculator

---
+++ Semester_1/Intro_to_cpp_for_dsa/simpleCalculator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x, y;
+    cin >> x >> y;
+
+    // sum = x + y;
+    // mul = x * y;
+    // sub = x - y;
+
+    cout << x << " + " << y << " = " << (x + y) << endl;
+    cout << x << " * " << y << " = " << (x * y) << endl;
+    cout << x << " - " << y << " = " << (x - y) << endl;
+
+    return 0;
+}
