05fedaf09d76b8c8620df583c58170716a3ee935
Arafat Islam Zihad <arafat.zihad01@gmail.com>
2025-06-26 23:05:20 +0600

    Max problem solved

--- Semester_1/Intro_to_cpp_for_dsa/Max.cpp
+++ Semester_1/Intro_to_cpp_for_dsa/Max.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int main(){
     int *n = new int;
-    cin 
+    cin >> *n;
+
+    int *a = new int [*n];
+
+    for(int i = 0; i < *n; i++){
+        cin >> a[i];
+    }
+
+    int max = 0;
+
+    for(int i = 0; i < *n; i++){
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+        
+    }
+    cout << max << endl;
+
+    
     return 0;
 }
