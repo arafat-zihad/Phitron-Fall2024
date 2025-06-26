a653f8c9011434399d117ce55fa9fb37856161a7
Arafat Islam Zihad <arafat.zihad01@gmail.com>
2025-06-26 20:26:13 +0600

    add dynamic array

--- Semester_1/Intro_to_cpp_for_dsa/reversing.cpp
+++ Semester_1/Intro_to_cpp_for_dsa/reversing.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
-    cin >> n;
+    int *n = new int;
+    cin >> *n;
 
-    int a[n];
-    for(int i = 0; i < n; i++){
+    int *a = new int[*n];
+
+    for(int i = 0; i < *n; i++){
         cin >> a[i];
     }
 
     // swap(a[0], a[n-1]);
 
-    for(int i = n-1; i >= 0; i--){
+    for(int i = *n-1; i >= 0; i--){
         cout << a[i] <<" ";
     }
     return 0;
