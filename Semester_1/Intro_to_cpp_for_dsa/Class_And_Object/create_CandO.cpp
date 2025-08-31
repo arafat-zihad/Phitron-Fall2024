e6e0b7b7bd1b34ee130b9cc33d69cf32a6857bef
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-31 08:34:41 +0600

    how to declare and execute class and object

--- Semester_1/Intro_to_cpp_for_dsa/Class_And_Object/create_CandO.cpp
+++ Semester_1/Intro_to_cpp_for_dsa/Class_And_Object/create_CandO.cpp
@@ -8,6 +8,12 @@ class Student{
 };
 
 int main(){
+    Student a;
+    a.roll = 52;
+    a.gpa = 3.85;
+    char temp[100] = "Zihad"; //We can't directly print string string
+    strcpy(a.name, temp);
     
+    cout << a.name << " " << a.roll << " " << a.gpa << endl;
     return 0;
 }
