9c6c6bac8eebf3f6be725d9228e03b7b5381b091
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-01 14:32:15 +0600

    dereferanceing

--- Semester_1/Intro_to_cpp_for_dsa/pointer.cpp
+++ Semester_1/Intro_to_cpp_for_dsa/pointer.cpp
@@ -5,5 +5,7 @@ int main(){
     int *ptr = &a;
     // cout << &a << endl;
     cout << ptr << endl; // pointer of a ptr, stored address of a.
+    // Dereferancing 
+    cout << *ptr << endl;
     return 0;
 }
