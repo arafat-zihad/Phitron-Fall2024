50901602bd5f65116576dad084a34384b71c7584
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-01 14:28:05 +0600

    pointer

---
+++ Semester_1/Intro_to_cpp_for_dsa/pointer.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a = 20;
+    int *ptr = &a;
+    // cout << &a << endl;
+    cout << ptr << endl; // pointer of a ptr, stored address of a.
+    return 0;
+}
