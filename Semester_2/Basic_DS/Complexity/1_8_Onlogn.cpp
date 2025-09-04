ed9067c2184fba55389a6d85241c0aced3bc5619
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-04 12:11:25 +0600

    o(nlogn)

--- Semester_2/Basic_DS/Complexity/1_8_Onlogn.cpp
+++ Semester_2/Basic_DS/Complexity/1_8_Onlogn.cpp
@@ -1,13 +1,15 @@
-//Condition is loop will be nested
+//Condition is loop will be nested and logN
 
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
     int n; cin >> n;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j*=2){
             cout << "Hello" << endl;
+
         }
+
     }
     return 0;
 }
