acbba35af5f0fb0790e2b4725dfa1286a6be6b8a
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-07 18:59:37 +0600

    vector inicialization

---
+++ Semester_2/Basic_DS/Vector/Vector_inicialization.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // vector<int> v(); - type 1
+    // vector<int> v(5); - type 2
+    // vector<int> v(5, 1); - type 3
+    // vector<int> v2(v); - type 4
+    // int arr[5] = {1, 2, 3, 4, 5}; - type 5
+    // vector<int> v(arr, arr+5);
+    vector<int> v = {9, 8, 7, 6, 5};
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] <<" "; 
+    }
+    
+    return 0;
+}
