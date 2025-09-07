3e2832864cc0641af9664c88665693eed2185769
arafat-zihad <arafat.zihad01@gmail.com>
2025-09-07 19:15:30 +0600

    shortcut for loop

--- Semester_2/Basic_DS/Vector/vector_modiiers.cpp
+++ Semester_2/Basic_DS/Vector/vector_modiiers.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main(){
     vector<int> v = {1, 2, 3, 4, 5};
     vector<int> v2 = v; 
-    for(size_t i = 0; i < v2.size(); i++){
-        cout << v2[i];
+    // for(size_t i = 0; i < v2.size(); i++){
+    //     cout << v2[i];
+    // }
+    for(int i : v2){
+        cout << i << " ";
     }
     return 0;
 }
