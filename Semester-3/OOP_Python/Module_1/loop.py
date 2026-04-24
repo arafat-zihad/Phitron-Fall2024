d67a41ea017a560103b2bc088a2f9dcef192c88e
arafat-zihad <arafat.zihad01@gmail.com>
2026-04-24 21:00:12 +0600

    add loop.py with example loops and print statements

---
+++ Semester-3/OOP_Python/Module_1/loop.py
@@ -0,0 +1,26 @@
+# num  = 1
+# while num < 10:
+#     print(num)
+#     num = num + 1
+
+# numbers = [5, 10, 15, 20, 25]
+
+# for i in numbers:
+#     print(i) 
+
+# text = 'Pagla Hower Tore'
+# for i in text:
+#     print(i)
+
+# Print 1 to 10
+# for i in range(1, 10+1):
+#     print(i)
+
+# Print in reverse order
+# for i in range(10, 0, -1):
+#     print(i)
+
+friend = ['Thaki', 'Tahmed', 'Ruhan', 'Zarif']
+for i in range(len(friend)):
+    print(f"{i} Name: {friend[i]}")
+
