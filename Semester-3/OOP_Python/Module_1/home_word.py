ff8bc08b91868c7d9c69e315211984f6444cb2ba
arafat-zihad <arafat.zihad01@gmail.com>
2026-05-04 11:26:35 +0600

    add home_word.py with homework tasks and instructions

---
+++ Semester-3/OOP_Python/Module_1/home_word.py
@@ -0,0 +1,9 @@
+""" 
+HOMEWORK
+
+1. float
+2. Take 3 numbers from the user and give me the largest number as output
+3. Take 3 numbers from the user and give me the sum of the numbers
+4. Run a loop and show me the odd numbers between 39 to 68
+5. Grade calculator in python (if, elif, else)
+"""
